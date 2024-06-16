7c2341c29095639c56d899014388ed2e9aa51df6
Ottar Kvindesland <149434896+LA9IHA@users.noreply.github.com>
2024-06-16 20:54:21 +0200

    Update main.py

--- bullen/artikler/3-Objekter/main.py
+++ bullen/artikler/3-Objekter/main.py
@@ -3,10 +3,10 @@ from funksjon import Funksjoner
 b = Funksjoner()
 while True:
     o = b.hentOrd()
-    if o == int('10', 2): b.putStreng("CQ CQ CQ de LA9IHA LA9IHA LA9IHA pse k")
-    elif o == int('100', 2) : b.putStreng("TNX FER CALL - UR RST RST RST es 599 599 5nn es name name es O t t a r  o t t a r   o t t a r - hw cpy?")
-    elif o == int('110', 2) : b.putStreng("vy happy xmas")
-#
-#    elif o == 2 : b.putStreng("CQ CQ CQ de LA9IHA LA9IHA LA9IHA pse k")
-#    elif o == 4 : b.putStreng("TNX FER CALL - UR RST RST RST es 599 599 5nn es name name es O t t a r  o t t a r   o t t a r - hw cpy?")
-#   elif o == 6 : b.putStreng("vy happy xmas")
+    if o > 0:
+        if o == 1: b.putStreng("CQ CQ CQ de LA9IHA LA9IHA LA9IHA pse k")
+        elif o == 2 : b.putStreng("TNX FER CALL - UR RST RST RST es 599 599 5nn es name name es O t t a r  o t t a r   o t t a r - hw cpy?")
+        elif o == 3 : b.putStreng("vy happy xmas")
+        else:
+            b.putStreng("? " + str(o))
+            
